945e7eae6a4947bf42e1393cca57d9b9037d2132
Leifur Sigurdsson <leifurerik@gmail.com>
2018-09-27 22:40:47 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board?(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def valid_move?(board, index)
