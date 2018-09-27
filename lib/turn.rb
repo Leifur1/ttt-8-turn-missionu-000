1606068002038cfbeb67ac7bf05226c4ad26c25d
Leifur Sigurdsson <leifurerik@gmail.com>
2018-09-27 23:05:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,17 @@ def display_board(board)
   puts " "+board[6]+" | "+board[7]+" | "+board[8]+" "
 end
 
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+
 def valid_move?(board, index)
   def position_taken?(board, index)
     if ((board[index] == " ") || (board[index] == "") || (board[index] == nil))
