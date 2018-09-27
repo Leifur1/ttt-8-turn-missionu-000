3f999f292874736f1a8bd8a2b7ce9da7274bcf78
Leifur Sigurdsson <leifurerik@gmail.com>
2018-09-27 23:01:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,9 +7,25 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if ((board[index] == " ") || (board[index] == "") || (board[index] == nil))
-    false
-  else
-    true
+  def position_taken?(board, index)
+    if ((board[index] == " ") || (board[index] == "") || (board[index] == nil))
+      false
+    else
+      true
+    end
   end
+
+  def on_board?(num-1)
+    if num.between(0,8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index) == false) && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end 
 end
